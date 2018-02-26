bfed975159a3d2ee466fe7f2d52b9cd3c61b57e6
MoongK <qorfyd01@gmail.com>
2018-02-26 17:53:56 +0900

    2DplatformerGame

---
+++ Assets/CoinState.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinState : MonoBehaviour {
+
+    bool coinroll = false;
+
+    Vector3 moveDir;
+    Vector3 upper, downner;
+
+    private void Start()
+    {
+        moveDir = transform.position;
+        moveDir.y = transform.position.y + 1f;
+
+        upper = transform.position;
+        upper.y = transform.position.y + 0.2f;
+
+        downner = transform.position;
+        downner.y = transform.position.y - 0.2f;
+    }
+
+    private void Update()
+    {
+        if (coinroll == true)
+            StartCoroutine(PickupCoin());
+        else
+        {
+            idleCoin();
+        }
+    }
+
+    IEnumerator PickupCoin()
+    {
+        if (transform.localScale.x == 1f)
+        {
+            yield return new WaitForSeconds(Time.deltaTime * 2f);
+            Vector3 CoinScale = transform.localScale;
+            CoinScale.x = 0.2f;
+            transform.localScale = CoinScale;
+        }
+        else
+        {
+            yield return new WaitForSeconds(Time.deltaTime * 2f);
+            Vector3 CoinScale = transform.localScale;
+            CoinScale.x = 1f;
+            transform.localScale = CoinScale;
+        }
+    }
+
+    void idleCoin()
+    {
+        if (transform.position.y >= upper.y)
+            moveDir = downner;
+        else if (transform.position.y <= downner.y)
+            moveDir = upper;
+
+        transform.position = Vector3.MoveTowards(transform.position, moveDir, 1f * Time.deltaTime);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            coinroll = true;
+            Destroy(gameObject, 1f);
+        }
+    }
+}
---
+++ Assets/DropPlatform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropPlatform : MonoBehaviour {
+
+    bool droping = false;
+
+    private void Update()
+    {
+        if (droping == true)
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + Vector3.down, 0.1f);
+    }
+
+    void Drop()
+    {
+        droping = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+            Invoke("Drop", 1f);
+    }
+}
